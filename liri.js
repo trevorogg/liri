dd4cd7fbcad6c279886be7659ef2dd883169f0d0
Trevor Ogg <trevorogg@Trevors-MacBook-Air.local>
2018-04-15 11:10:28 -0600

    added NPM packages

--- liri.js
+++ liri.js
@@ -1,1 +1,12 @@
+var fs = require('fs');
+var keys = require('./keys.js');
+var request = require('request');
+//var spotify = require('spotify');
+var twitter = require('twitter');
+var input = process.argv[2];
+
 require("dotenv").config();
+
+
+// var spotify = new Spotify(keys.spotify);
+// var client = new Twitter(keys.twitter);
