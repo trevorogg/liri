a4b4deb50893175deb4f6b41412f834c95bfb385
Trevor Ogg <trevorogg@Trevors-MacBook-Air.local>
2018-04-21 21:54:03 -0600

    working spotify and twitter with inquirer

--- liri.js
+++ liri.js
@@ -3,17 +3,44 @@ require("dotenv").config();
 var fs = require('fs');
 var keys = require('./keys.js');
 var request = require('request');
+var inquirer = require("inquirer");
 
-//var spotify = require('spotify');
+var Spotify = require('node-spotify-api');
 
 const Twitter = require('twitter');
 const TwitterUser = 'TrevorOgg';
 
-// variable for user input
 var input = process.argv[2];
 
-// var spotify = new Spotify(keys.spotify);
+var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
+
+function liri() {
+    inquirer
+        .prompt({
+            name: 'action',
+            type: 'rawlist',
+            message: 'What would you like to do?',
+            choices: [
+                'Show tweets',
+                'Find a song',
+                'Find a movie',
+                'Do what it says'
+            ]
+        })
+        .then(function (answer) {
+            switch(answer.action){
+                case 'Show tweets':
+                showTweets();
+                break;
+
+                case 'Find a song':
+                findSong();
+                break;
+            }
+
+        });
+};
 
 function showTweets() {
 
@@ -36,8 +63,40 @@ function showTweets() {
     });
 };
 
-showTweets();
+function findSong(songName) {
 
+    inquirer
+        .prompt({
+            name: 'action',
+            type: 'input',
+            message: 'What song?',
+        })
+        .then(function (answer) {
 
+            if (!songName) {
+                songName = "The Sign";
+            }
+            params = answer;
+            spotify.search({ type: "track", query: params }, function (err, data) {
+                if (!err) {
+                    var songInfo = data.tracks.items;
+                    for (var i = 0; i < 5; i++) {
+                        if (songInfo[i] != undefined) {
+                            var spotifyResults =
+                                "Artist: " + songInfo[i].artists[0].name + "\r\n" +
+                                "Song: " + songInfo[i].name + "\r\n" +
+                                "Album the song is from: " + songInfo[i].album.name + "\r\n" +
+                                "Preview Url: " + songInfo[i].preview_url + "\r\n" +
+                                "------------------------------ " + i + " ------------------------------" + "\r\n";
+                            console.log(spotifyResults);
+                        }
+                    }
+                } else {
+                    console.log("Error :" + err);
+                    return;
+                }
+            });
+        });
+};
 
-
+liri();
